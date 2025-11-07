cd7f7ca2fd16d9fdc71613ba860015545e885ebf
danijelhusakovic <danijel.husakovic@gmail.com>
2025-11-07 12:03:31 +0100

    Vjezba02 - Prvi program

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule],
 })
 export class HomePage {
-  constructor() {}
+  tekst = "Pocetni tekst";
+
+  onChangeText() {
+    this.tekst = "Tekst promijenjen!";
+  }
 }
